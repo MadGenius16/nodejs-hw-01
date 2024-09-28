02486a1171547e58c47657a9821031e6b9f3d738
MadGenius16 <aleksandrco777@gmail.com>
2024-09-28 21:13:25 +0300

    крок 3

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const buffer = await fs.readFile(PATH_DB, 'utf-8');
+    const data = JSON.parse(buffer);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
