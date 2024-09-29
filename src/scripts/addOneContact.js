51ba8465b514fda965d13b0e21e958fae05e2c65
MadGenius16 <aleksandrco777@gmail.com>
2024-09-29 17:36:32 +0300

    крок 5

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+  const contact = await readContacts();
+  contact.push(createFakeContact());
+  await writeContacts(contact);
+};
 
 addOneContact();
