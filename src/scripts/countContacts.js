f90fbe6e0f014d1e489aa40a44bdb80482fc36e4
MadGenius16 <aleksandrco777@gmail.com>
2024-09-29 19:50:37 +0300

    крок 8

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,8 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  const contactsArr = await readContacts();
+  return contactsArr.length;
+};
 
 console.log(await countContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,13 @@
-export const removeLastContact = async () => {};
+
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if(contacts !==0) contacts.pop();
+  else return;
+  await writeContacts(contacts);
+};
 
 removeLastContact();
